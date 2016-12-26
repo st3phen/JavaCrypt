b4988811202e3ce8301a485a9a0d7e71c5269d62
Stephen <stephen@nowhere>
2016-12-26 14:13:26 -0500

    Added some algorithm notes

--- CryptoLib.java
+++ CryptoLib.java
@@ -1,5 +1,5 @@
 /**
- * A helper library for JavaCrypt which contains implementations of 
+ * A helper library for JavaCrypt which contains implementations of
  * CubeHash, RC4, and TEA.
  *
  * @author Stephen Solis
@@ -9,10 +9,10 @@ public class CryptoLib {
 
 	/**
 	 * CubeHash is one of the most simple yet cryptographically strong hash
-	 * functions that exist. The algorithm was designed by Daniel J. Bernstein 
+	 * functions that exist. The algorithm was designed by Daniel J. Bernstein
 	 * (under funding by NIST) and is in the public domain.
 	 * <br>
-	 * This is an implementation of CubeHash 16/32-512, which was a 2nd-round 
+	 * This is an implementation of CubeHash 16/32-512, which was a 2nd-round
 	 * SHA-3–512-normal candidate.
 	 * (16 = # of rounds, 32 = block size (in bytes), 512 = output size (in bits))
 	 * <br>
@@ -32,7 +32,7 @@ public class CryptoLib {
 			state[1] = 32;
 			state[2] = 16;
 			rounds(16); //do 16 initialization rounds
-			//convert the input to integers (makes logical operations easier), then pad it 
+			//convert the input to integers (makes logical operations easier), then pad it
 			//as described in the algorithm
 			//first, compute size of input[]
 			int inputSize = message.length;
@@ -45,12 +45,12 @@ public class CryptoLib {
 			//convert inputBytes (a byte[]) to input (an int[])
 			int[] input = new int[inputSize/4]; //an int has length 4 bytes
 			for(int i = 0; i < inputSize; i += 4) {
-				input[i/4] = ((inputBytes[i]&0xFF)) | 
-							 ((inputBytes[i+1]&0xFF) << 8) | 
-							 ((inputBytes[i+2]&0xFF) << 16) | 
+				input[i/4] = ((inputBytes[i]&0xFF)) |
+							 ((inputBytes[i+1]&0xFF) << 8) |
+							 ((inputBytes[i+2]&0xFF) << 16) |
 							 ((inputBytes[i+3]&0xFF) << 24);
 			}
-		
+
 			/* STAGE 2 - input processing phase */
 			for (int block = 0; block < input.length; block += 8) {
 				for (int i = 0; i < 8; i++) {
@@ -59,7 +59,7 @@ public class CryptoLib {
 				}
 				rounds(16); //do 16 rounds per message block
 			}
-		
+
 			/* STAGE 3 - finalization phase */
 			state[31] ^= 1; //xor 1 into last state word
 			rounds(32); //32 finalization rounds
@@ -73,7 +73,7 @@ public class CryptoLib {
 			}
 			return output;
 		}
-		
+
 		/**
 		 * Performs n full CubeHash rounds on state[].
 		 * @param n	the number of rounds to perform
@@ -87,7 +87,7 @@ public class CryptoLib {
 				swap_xor_swap(4, 1); //8-10
 			}
 		}
-		
+
 		/**
 		 * Performs an 'add, rotate' step on state[].
 		 */
@@ -97,19 +97,19 @@ public class CryptoLib {
 				state[i] = (state[i] << r) ^ (state[i] >>> s); //..then rotate
 			}
 		}
-		
+
 		/**
 		 * Performs a 'swap, xor, swap' step on state[].
 		 */
 		private static void swap_xor_swap (int mask1, int mask2) {
 			//the step numbers referenced are from http://cubehash.cr.yp.to
-			
+
 			int tmp, j; //temp variables
 			for (int i = 0; i < 16; i++) {
 				if ((i & mask1) != 0) { //does the [00klm] (step 3) or [0j0lm] (step 8) masks
 					//swap + xor at the same time..
 					j = i ^ mask1;
-					tmp = state[i] ^ state[j + 16]; 
+					tmp = state[i] ^ state[j + 16];
 					state[i] = state[j] ^ state[i + 16];
 					state[j] = tmp;
 				}
@@ -125,16 +125,16 @@ public class CryptoLib {
 			}
 		}
 	}
-	
+
 	/**
-	 * RC4 is a very popular, very simple, very fast stream cypher designed 
-	 * by Ron Rivest at RSA in 1987. It is not copyrighted, but was a trade 
+	 * RC4 is a very popular, very simple, very fast stream cypher designed
+	 * by Ron Rivest at RSA in 1987. It is not copyrighted, but was a trade
 	 * secret until 1994, when it was leaked.
 	 * <br>
 	 * See http://en.wikipedia.org/wiki/RC4 for more details.
 	 */
 	public static class RC4 {
-	
+
 		/**
 		 * Performs RC4 encryption/decryption (RC4 is symmetric!) on the given
 		 * data with the given key.
@@ -145,18 +145,18 @@ public class CryptoLib {
 		public static byte[] doRC4(byte[] key, byte[] data) {
 			int[] K = new int[256]; //holds the RC4 key array
 			byte[] output = new byte[data.length]; //holds the output
-			
+
 			//convert key[] to an int[] mod 256, and store in keyInt[]
 			int[] keyInt = new int[key.length];
 			for (int i = 0; i < key.length; i++){
 				//I have absolutely no idea why Java doesn't have an unsigned
 				//int, so I have to do the conversion manually...
 				if ((int)key[i] >= 0)
-					keyInt[i] = (int)key[i]; 
+					keyInt[i] = (int)key[i];
 				else
 					keyInt[i] = (int)key[i] + 256;
 			}
-			
+
 			//initialize the key array K
 			for (int i = 0; i < 256; i++) {
 				K[i] = i; //write numbers 0 to 256 into K
@@ -166,37 +166,37 @@ public class CryptoLib {
 			for (int i = 0; i < 256; i++) {
 				a = (a + K[i] + keyInt[i % keyInt.length]) % 256;
 				//swap K[i] with K[a]
-				temp = K[i]; 
+				temp = K[i];
 				K[i] = K[a];
 				K[a] = temp;
 			}
-	
-			/* once the key array has been generated, use the pseudo-random 
-			   byte generator specified in the spec and xor those values with 
+
+			/* once the key array has been generated, use the pseudo-random
+			   byte generator specified in the spec and xor those values with
 			   the input bytestream */
 			int b = 0, c = 0; //from the spec
-	
+
 			for (int k = 0; k < data.length; k++) { //iterate through each input byte
 				b = (b + 1) % 256;
 				c = (c + K[b]) % 256;
 				//swap K[b] with K[c]
-				temp = K[b]; 
+				temp = K[b];
 				K[b] = K[c];
 				K[c] = temp;
-				//xor K[(K[b] + K[c]) mod 256] with the current input byte 
+				//xor K[(K[b] + K[c]) mod 256] with the current input byte
 				//and store it in output[k]
 				//note: Java only does bitwise operations with ints, so we have
 				//to cast to int and back to a byte at the end
 				output[k] = (byte)((int)data[k] ^ (int)K[(K[b] + K[c]) % 256]);
 			}
-	
+
 			return output;
 		}
 	}
-	
+
 	/**
-	 * TEA (Tiny Encryption Algorithm) was designed by David Wheeler and 
-	 * Roger Needham of the Cambridge Computer Laboratory in 1994, and was 
+	 * TEA (Tiny Encryption Algorithm) was designed by David Wheeler and
+	 * Roger Needham of the Cambridge Computer Laboratory in 1994, and was
 	 * designed to be the simplest (to implement) encryption algorithm ever.
 	 * It has an unbalanced Feistel structure of 64 rounds. The algorithm and
 	 * some reference implementations are in the public domain.
@@ -207,10 +207,10 @@ public class CryptoLib {
 	 */
 	public static class TEA {
 		private static final int DELTA = 0x9E3779B9; //from the spec
-	
+
 		private static int[] subKeys = new int[4]; //see XTEA_init()
 		private static int v0, v1; //from the spec
-	
+
 		/**
 		 * Performs TEA encryption on the given data with the given key.
 		 * @param key	the key to use - must be 16 bytes/128 bits long
@@ -222,7 +222,7 @@ public class CryptoLib {
 			XTEA_encrypt();
 			return XTEA_finalize();
 		}
-		
+
 		/**
 		 * Performs TEA decryption on the given data with the given key.
 		 * @param key	the key to use - must be 16 bytes/128 bits long
@@ -234,7 +234,7 @@ public class CryptoLib {
 			XTEA_decrypt();
 			return XTEA_finalize();
 		}
-		
+
 		/**
 		 * Initializes subKeys[], v0, and v1 with data from the given key[]
 		 * and data[].
@@ -243,24 +243,24 @@ public class CryptoLib {
 			//first, convert the byte[] key to 4 integers in subKeys:
 			//this makes doing logical operations much easier
 			for(int i = 0; i < 16; i += 4) {
-				subKeys[i/4] = ((key[i]&0xFF)) | 
-								((key[i+1]&0xFF) << 8) | 
-								((key[i+2]&0xFF) << 16) | 
+				subKeys[i/4] = ((key[i]&0xFF)) |
+								((key[i+1]&0xFF) << 8) |
+								((key[i+2]&0xFF) << 16) |
 								((key[i+3]&0xFF) << 24);
 			}
-		
+
 			//then, convert the byte[] data to two integers, v0 and v1 (as in the spec)
-			v0 = ((data[0] & 0xFF)) | 
-				 ((data[1] & 0xFF) <<  8) | 
-				 ((data[2] & 0xFF) << 16) | 
+			v0 = ((data[0] & 0xFF)) |
+				 ((data[1] & 0xFF) <<  8) |
+				 ((data[2] & 0xFF) << 16) |
 				 ((data[3]) << 24);
-			
-			v1 = ((data[4] & 0xFF)) | 
-				 ((data[5] & 0xFF) <<  8) | 
-				 ((data[6] & 0xFF) << 16) | 
+
+			v1 = ((data[4] & 0xFF)) |
+				 ((data[5] & 0xFF) <<  8) |
+				 ((data[6] & 0xFF) << 16) |
 				 ((data[7]) << 24);
 		}
-		
+
 		/**
 		 * Performs XTEA encryption on v0 and v1 using subKeys[].
 		 * This is straight from the spec.
@@ -274,7 +274,7 @@ public class CryptoLib {
 				v1 += ((v0 << 4 ^ v0 >>> 5) + v0) ^ (sum + subKeys[sum >> 11 & 3]);
 			}
 		}
-		
+
 		/**
 		 * Performs XTEA decryption on v0 and v1 using subKeys[].
 		 * This is straight from the spec.
@@ -288,14 +288,14 @@ public class CryptoLib {
 				v0 -= ((v1 << 4 ^ v1 >>> 5) + v1) ^ (sum + subKeys[sum & 3]);
 			}
 		}
-	
+
 		/**
 		 * Converts v0 and v1 back to a byte[].
 		 * @return	a byte[] representing v0 and v1
 		 */
 		private static byte[] XTEA_finalize(){
 			byte[] out = new byte[8];
-			
+
 			//v0 first..
 			out[0] = (byte)(v0);
 			out[1] = (byte)(v0 >>> 8);
@@ -306,7 +306,7 @@ public class CryptoLib {
 			out[5] = (byte)(v1 >>> 8);
 			out[6] = (byte)(v1 >>> 16);
 			out[7] = (byte)(v1 >>> 24);
-			
+
 			return out;
 		}
 	}
--- JavaCrypt.java
+++ JavaCrypt.java
@@ -5,13 +5,36 @@ import java.security.*;
 /**
  * JavaCrypt: An encryption program that uses RC4 and TEA.
  *
+ * The algorithm is as follows:
+ *   To encrypt:
+ *		1. Create an encryption key (512 bits): CubeHash(password + #padded bits (see step 2) + random salt (128 bytes))
+ *			-The salt prevents using rainbow tables
+ *		2. Encrypt 64-bit chunks of data using 128-bit chunks of the key:
+ *			-RC4, then TEA:
+ *			 TEA(key, RC4(key, data))
+ *		   If the chunk is the last one and has length < 64, pad it using random data
+ *		3. Write data to the file as follows:
+ *			/x01/x02/x03/x04/x05/x06	<- marker so I know the file is valid
+ *			ABBBBBBBBBBBBBBBBB			<- A  = # of padded bits added to make 64-bit chunk at the end
+ *										   B* = random salt (256 bits)
+ *			xxxxxxxxxxxxxxxxxxx			<- x* = encrypted data
+ *   *****************************************************************************************
+ *   To decrypt:
+ *		1. Check the starting marker to see if the file is valid
+ *		2. Regenerate the encryption key: CubeHash(inputted password + 2nd line from file)
+ *		3. Decrypt 64-bit chunks of data using 128-bit chunks of the key:
+ *			-unTEA, then RC4 (RC4 is symmetric!):
+ *			 RC4(key, unTEA(key, data))
+ *		4. Delete the padding from the last 64-bit chunk only.
+ *		5. Write the decrypted data back to the file.
+ *
  * @author Stephen Solis
  * @version 03/22/2012
  */
-public class Main {
+public class JavaCrypt {
 	/** This header is written by encrypt() and checked by decrypt() */
 	private static final byte[] FILE_HEADER = {0x01, 0x02, 0x03, 0x04};
-	
+
 	private static String inFilename;
 	private static String outFilename;
 	private static byte[] password;
@@ -19,20 +42,20 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		boolean doEncrypt = getUserChoice();
 		getFilenameAndPassword();
-		
+
 		if (doEncrypt)
 			encrypt();
 		else
 			decrypt();
 	}
-	
+
 	/**
 	 * Asks the user whether they wish to encrypt or decrypt a file.
 	 * @return true if the user wishes to encrypt, false if the user wishes to decrypt
 	 */
 	public static boolean getUserChoice() throws IOException{
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		while (true){
 			//print the message
 			System.out.println();
@@ -41,9 +64,9 @@ public class Main {
 			System.out.println("2. Decrypt a file.");
 			System.out.println("3. Quit.");
 			System.out.print("[1/2/3]: ");
-			
+
 			//perform the action the user requests
-			try { 
+			try {
 				int userChoice = Integer.parseInt(in.readLine());
 				if (userChoice  == 1) //user chooses "Encrypt a file."
 					return true;
@@ -58,7 +81,7 @@ public class Main {
 			}
 		}
 	}
-	
+
 	/**
 	 * Sets the global {in,out}Filename and password variables for encrypt()
 	 * and decrypt() to use.
@@ -66,16 +89,16 @@ public class Main {
 	public static void getFilenameAndPassword() throws IOException{
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		Console console = System.console();
-		
+
 		boolean validFilename = false;
 		String currInput; //holds the user's input before we check it
-		
+
 		//get the input filename
 		System.out.println(); //make output look neat
 		while (!validFilename){
 			//print the message
 			System.out.print("Enter input filename: ");
-			
+
 			//check if the file exists.
 			currInput = in.readLine();
 			File file = new File(currInput);
@@ -88,13 +111,13 @@ public class Main {
 				validFilename = true;
 			}
 		}
-		
+
 		//do the same for the output filename
 		validFilename = false;
 		while (!validFilename){
 			//print the message
 			System.out.print("Enter output filename: ");
-			
+
 			//check if the file exists.
 			currInput = in.readLine();
 			File file = new File(currInput);
@@ -108,35 +131,35 @@ public class Main {
 				validFilename = true;
 			}
 		}
-		
+
 		//get the password
 		System.out.print("Enter password: ");
 		char[] passwordChar = console.readPassword();
-		
+
 		//convert the password from chars to a bytestream (ie. ASCII to binary)
 		password = new byte[passwordChar.length];
 		for (int i = 0; i < passwordChar.length; i++){
 			password[i] = (byte)passwordChar[i];
 		}
 	}
-	
+
 	/**
-	 * Encrypts the file specified in inFilename and puts the result in the 
+	 * Encrypts the file specified in inFilename and puts the result in the
 	 * file specified by outFilename.
 	 */
 	public static void encrypt() throws IOException{
 		BufferedInputStream in = new BufferedInputStream(new FileInputStream(inFilename));
 		BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(outFilename));
-		
+
 		SecureRandom randomGen = new SecureRandom(); //used for the salt and the padding
 		byte[] key; //holds the encryption key
-		
+
 		//get the file length
 		long fileLength = (new File(inFilename)).length();
-		
+
 		//write the file header marker
 		out.write(FILE_HEADER);
-		
+
 		/* STAGE 1 - generate encryption key */
 		//find the number of bytes we have to pad and write it to the file
 		int numPad;
@@ -156,30 +179,30 @@ public class Main {
 		System.arraycopy(salt, 0, message, password.length + 1, salt.length); //add random salt
 		//finally, generate the encryption key
 		key = CryptoLib.CubeHash.doCubeHash(message);
-		
+
 		/* STAGE 2 - apply encryption */
 		int currPiece = 0;  //keeps track of which piece of the key is being used
 		byte[] currData = new byte[8]; //holds the piece of data currently being encrypted
-		
+
 		//split the key into 4 pieces of 128 bits each
 		byte[][] keyPieces = new byte[4][16];
 		for (int i = 0; i < 4; i++){
 			System.arraycopy(key, i*16, keyPieces[i], 0, 16);
 		}
-		
+
 		//loop through the file (except the last block), 8 bytes at a time
 		for (long i = 0; i < fileLength - 8; i += 8){
 			//put a chunk of data in currData
 			in.read(currData);
 
 			//do TEA(key, RC4(key, data)), then write to file
-			out.write(CryptoLib.TEA.doTEA(keyPieces[currPiece], 
+			out.write(CryptoLib.TEA.doTEA(keyPieces[currPiece],
 						CryptoLib.RC4.doRC4(keyPieces[currPiece], currData)));
-			
+
 			//increment currPiece
 			currPiece = (currPiece + 1) % 4;
 		}
-		
+
 		//deal with the last block of data:
 		//first, read it..
 		in.read(currData);
@@ -188,28 +211,28 @@ public class Main {
 			currData[i] = (byte)randomGen.nextInt();
 		}
 		//..then encrypt it
-		out.write(CryptoLib.TEA.doTEA(keyPieces[currPiece], 
+		out.write(CryptoLib.TEA.doTEA(keyPieces[currPiece],
 					CryptoLib.RC4.doRC4(keyPieces[currPiece], currData)));
-		
+
 		/* STAGE 3 - done! */
 		in.close();
 		out.close();
 		System.out.println("\nEncryption complete.");
 	}
-	
+
 	/**
-	 * Decrypts the file specified in inFilename and puts the result in the 
+	 * Decrypts the file specified in inFilename and puts the result in the
 	 * file specified by outFilename.
 	 */
 	public static void decrypt() throws IOException{
 		BufferedInputStream in = new BufferedInputStream(new FileInputStream(inFilename));
 		BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(outFilename));
-	
+
 		byte[] key; //holds the encryption key
-	
+
 		//get the file length
 		long fileLength = (new File(inFilename)).length();
-	
+
 		//check the file header marker
 		byte[] actualHeader = new byte[FILE_HEADER.length];
 		in.read(actualHeader); //read header from the file
@@ -217,7 +240,7 @@ public class Main {
 			System.out.println("The file was not encrypted with this program.");
 			System.exit(-1); //-1 is an error code
 		}
-		
+
 		/* STAGE 1 - generate decryption key */
 		//read the #padded bits and random salt from the file
 		byte[] dataFromFile = new byte[129]; //1 byte for #padded bits + 128 bytes for salt
@@ -230,39 +253,39 @@ public class Main {
 		System.arraycopy(dataFromFile, 0, message, password.length, dataFromFile.length); //add dataFromFile
 		//finally, (re)generate the encryption key
 		key = CryptoLib.CubeHash.doCubeHash(message);
-		
+
 		/* STAGE 2 - apply decryption */
 		int currPiece = 0; //keeps track of which piece of the key is being used
 		byte[] currData = new byte[8]; //holds the data currently being decrypted
-		
+
 		//split the key into 4 pieces of 128 bits each
 		byte[][] keyPieces = new byte[4][16];
 		for (int i = 0; i < 4; i++){
 			System.arraycopy(key, i*16, keyPieces[i], 0, 16);
 		}
-		
+
 		//loop through the rest of the file (except the last block), 8 bytes at a time
 		for (long i = FILE_HEADER.length + 129; i < fileLength - 8; i += 8){
 			//put a chunk of data in currData
 			in.read(currData);
 
 			//do RC4(key, unTEA(key, data)), then write to file
-			out.write(CryptoLib.RC4.doRC4(keyPieces[currPiece], 
+			out.write(CryptoLib.RC4.doRC4(keyPieces[currPiece],
 						CryptoLib.TEA.doUnTEA(keyPieces[currPiece], currData)));
-			
+
 			//increment currPiece
 			currPiece = (currPiece + 1) % 4;
 		}
-		
+
 		//deal with the last chunk of data:
 		//first, read it..
 		in.read(currData);
 		//..decrypt it..
-		currData = CryptoLib.RC4.doRC4(keyPieces[currPiece], 
+		currData = CryptoLib.RC4.doRC4(keyPieces[currPiece],
 				CryptoLib.TEA.doUnTEA(keyPieces[currPiece], currData));
 		//..then write it to the file
 		out.write(currData, 0, (8 - numPad));
-				
+
 		/* STAGE 3 - done!  */
 		in.close();
 		out.close();
